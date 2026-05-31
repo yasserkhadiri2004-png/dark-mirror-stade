7e56a2cd95a7547f72d083141cf42ce356f16515
KHADIRI YASSER <yasserkhadiri2004@gmail.com>
2026-05-31 18:54:54 +0100

    Fix theme sphinx_rtd_theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,11 +6,13 @@ copyright = '2026, Yasser KHADIRI - ENSAM Meknes'
 author    = 'Yasser KHADIRI'
 release   = '1.0.0'
 
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon',
-]
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
 
 exclude_patterns = []
 html_theme       = 'sphinx_rtd_theme'
+html_logo        = None
+
+html_theme_options = {
+    'navigation_depth': 4,
+    'style_nav_header_background': '#C1272D',
+}
